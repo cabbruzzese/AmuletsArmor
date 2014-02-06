a28cdfdd3657dfad82ff35258e54d897a85c6f8c
cabbruzzese <cabbruzz@gmail.com>
2014-02-06 16:19:38 -0500

    Revert "Minor h/r regen modification"
    This reverts commit 0c03c6058a4afbabe066aa9a77d62557879e55ae.

--- Source/STATS.C
+++ Source/STATS.C
@@ -1157,7 +1157,7 @@ T_void StatsUpdatePlayerStatistics (T_void)
 			hrvalue = -20;
 
 			//check if player is moving
-			if (PlayerIsMoving() == TRUE)  
+			if (PlayerIsMoving())  
 			{
 				//make heartrate recovery slower
 				hrvalue += 10;
@@ -1199,23 +1199,10 @@ T_void StatsUpdatePlayerStatistics (T_void)
 			//adjust by percentage of heartrate over normal tick time to avoid increase in regeneration
 			hregen *= normaltimeratio;
 
-			//if relaxed
 			if (G_activeStats->HeartRate == 0)
 			{
-				//and not moving
-				if (PlayerIsMoving() == FALSE)
-				{
-					//big regen boost
-					mregen *= (float)1.5;
-					hregen *= (float)1.5;
-				}
-				//otherwise
-				else
-				{
-					//small regen boost
-					mregen *= (float)1.2;
-					hregen *= (float)1.2;
-				}
+				mregen *= 1.5;
+				hregen *= 1.5;
 			}
 
 //          MessagePrintf ("hregen=%f mregen=%f",hregen,mregen);
